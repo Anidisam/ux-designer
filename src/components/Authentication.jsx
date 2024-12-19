02a64937ac3b71f0a0a2701b5c66f254ea9b5d05
anidisamuel2006@gmail.com <anidisamuel2006@gmail.com>
2024-12-19 09:04:42 -0800

    final change

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -6,13 +6,9 @@ const Authentication = () => {
   return (
     <div className="auth-container">
       <h1>Welcome to Our Platform</h1>
-      <p>Please choose an option to continue:</p>
       <div className="auth-options">
         <Link to="/signin">
           <button className="auth-btn">Sign In</button>
-        </Link>
-        <Link to="/signup">
-          <button className="auth-btn">Sign Up</button>
         </Link>
         <Link to="/create-account">
           {/* <button className="auth-btn">Create Account</button> */}
